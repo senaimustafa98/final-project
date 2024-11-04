069601b0442ac5aae0b9c57fe88ea4ec0da3e929
Senay Mustafa <senaim98@gmail.com>
2024-11-04 20:06:49 +0100

    Try fix psql issue

--- util/config.js
+++ util/config.js
@@ -3,7 +3,7 @@ import postgres from 'postgres';
 
 // Define PostgreSQL configuration
 export const postgresConfig = {
-  ssl: Boolean(process.env.POSTGRES_URL),
+  ssl: false,
   transform: {
     ...postgres.camel,
     undefined: null,
@@ -12,15 +12,5 @@ export const postgresConfig = {
 
 // Function to set environment variables
 export function setEnvironmentVariables() {
-  if (process.env.NODE_ENV === 'production' || process.env.CI) {
-    if (process.env.POSTGRES_URL) {
-      process.env.PGHOST = process.env.POSTGRES_HOST;
-      process.env.PGDATABASE = process.env.POSTGRES_DATABASE;
-      process.env.PGUSER = process.env.POSTGRES_USER;
-      process.env.PGPASSWORD = process.env.POSTGRES_PASSWORD;
-    }
-    return;
-  }
-  // Load .env for development
   config();
 }
